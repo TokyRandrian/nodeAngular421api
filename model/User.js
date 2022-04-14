abc052266cd6d7df07e5928219aa0a3e1966de7b
Andy Andrianjatovo <aandrianjatovoniaina@afiassurances.fr>
2022-04-14 15:30:59 +0200

    first

---
+++ model/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    nom: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 255
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 1024
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
